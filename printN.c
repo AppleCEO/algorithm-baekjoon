354df7a00a7c7e8f63e093c8bbfb947b78c2ca05
kiljh-me <47517256+kiljh-me@users.noreply.github.com>
2019-02-28 09:36:24 +0900

    i+1 change i
    print i+1 is lazy.
    print i is clear.

--- printN.c
+++ printN.c
@@ -4,8 +4,8 @@ int main(){
     int i, N;
     scanf("%d", &N);
     
-    for(i=0; i<N; i++){
-        printf("%d\n", i+1);    
+    for(i=1; i<=N; i++){
+        printf("%d\n", i);    
     }
     
     return 0;
