3e4559a530de6d16f7c9ad1da89d60eb8c342feb
kiljh <kiljh@me.com>
2019-05-24 15:05:27 +0900

    1000번 문제 구현

---
+++ algorithm/algorithm/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  algorithm
+//
+//  Created by joon-ho kil on 5/24/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import Foundation
+
+enum InputError: Error {
+    case isNotInt
+}
+
+func main() throws {
+    let input:String = readLine() ?? ""
+    let inputs = input.split(separator: " ")
+    guard let A = Int(inputs[0]), let B = Int(inputs[1]) else {
+        throw InputError.isNotInt
+    }
+    print(A+B)
+}
+
+try main()
